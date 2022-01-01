69fc37525b535813099c24d7aeeb7e8fc54a556e
lee <anomalocaris3871@gmail.com>
2022-01-01 19:40:41 +0900

    Initial commit

---
+++ src/main/java/com/housekeepingbook/housekeepingbook/HousekeepingbookApplication.java
@@ -0,0 +1,13 @@
+package com.housekeepingbook.housekeepingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeepingbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousekeepingbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/housekeepingbook/housekeepingbook/HousekeepingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housekeepingbook.housekeepingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousekeepingbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
